2dab6e2db87cb4a440989a704bc9e755dba71c20
kimmyeunghoe <kim6515516@gmail.com>
2015-11-19 13:09:01 +0900

    Initial commit

---
+++ app/src/main/java/com/example/kimmyeunghoe/musick/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.kimmyeunghoe.musick;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    Button btStart;
+    Button btStop;
+    MediaPlayer mMplayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMplayer = new MediaPlayer();
+
+        // Prepare mp3 file for MediaPlayer
+        try {
+            AssetFileDescriptor afd = getResources().openRawResourceFd(R.raw.boysandgirls);
+            mMplayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+        } catch(Exception e) {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+
+        btStart = (Button) findViewById(R.id.bt_start);
+        btStop = (Button) findViewById(R.id.bt_stop);
+
+
+
+        btStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    mMplayer.prepare();
+                    mMplayer.start();
+                } catch(Exception e) {
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mMplayer.stop();
+            }
+        });
+
+    }
+}
